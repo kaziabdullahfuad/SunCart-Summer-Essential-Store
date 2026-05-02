1d70afcd833f30fd08e0f58bc6dad243d65512ab
Kazi Abdullah Fuad <kaziabdullahfuad@gmail.com>
2026-05-03 03:02:15 +0600

    Unsplahs trying now

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-       
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
         hostname: "ibb.co.com",
-
       },
     ],
   },
